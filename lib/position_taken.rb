87667f2d64c7c6a7078a88a55b4ede9dca2ef4a0
Austen Radcliff <austen.radcliff@wework.com>
2018-02-21 03:17:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,13 @@
 # code your #position_taken? method here!
 
+Array[8]
+
 def display_board(board,index)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-
-Array[8]
 
 def position_taken?(board, index)
   if index.to.i == position_taken?
