9193978e445c763abcdba14e1fc22f2e25fc93fa
Austen Radcliff <austen.radcliff@wework.com>
2018-02-21 03:22:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -19,3 +19,16 @@ def position_taken?(board, index)
     false
   end
 end
+
+def position_taken?(board,position)
+  if board[position] == " "
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
+    false
+    else   board[position] == "X" || "O"
+    true
+
+  end
+end
