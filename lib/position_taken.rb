57fddd04c4e59619bbcc23514f8f3d719646924e
Austen Radcliff <austen.radcliff@wework.com>
2018-02-21 03:20:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,14 +4,6 @@ Array[8]
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board(board,index)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
 def position_taken?(board, index)
   if index.to.i == position_taken?
     true
